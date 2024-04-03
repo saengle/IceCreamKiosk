a1ddc30675e3c01e79080d9ced4aaf27d3503674
saengle <zohv817@gmail.com>
2024-04-03 12:44:02 +0900

    Add: PerchasedIceCreamViewController

---
+++ IceCreamKiosk/PerchasedIceCreamViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PerchasedIceCreamController.swift
+//  IceCreamKiosk
+//
+//  Created by 쌩 on 4/3/24.
+//
+
+import UIKit
+
+class PerchasedIceCreamViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
--- IceCreamKiosk/ViewController.swift
+++ IceCreamKiosk/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tempButton(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
