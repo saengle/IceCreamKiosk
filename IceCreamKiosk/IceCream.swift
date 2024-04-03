d23490619704c1f706c55447814bc5b040597ecc
swimmmmmmm <khan991117@gmail.com>
2024-04-03 20:43:36 +0900

    [feat] basket VC UI 구현
    storyboard를 사용한 장바구니 페이지 구현

---
+++ IceCreamKiosk/IceCream.swift
@@ -0,0 +1,12 @@
+//
+//  IceCream.swift
+//  IceCreamKiosk
+//
+//  Created by 서수영 on 4/3/24.
+//
+
+import Foundation
+
+//struct IceCream {
+//    var
+//}
--- IceCreamKiosk/ViewController.swift
+++ IceCreamKiosk/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BasketViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var basketCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        basketCollectionView.delegate = self
+        basketCollectionView.dataSource = self
     }
 
+    @IBAction func cancelButtonAction(_ sender: Any) {
 
+        //list reset & navigation back
+    }
+
+    @IBAction func purchaseButtonAction(_ sender: Any) {
+        //alert
+        let sheet = UIAlertController(title: "구매", message: "구매하시겠습니까?", preferredStyle: .alert)
+
+        sheet.addAction(UIAlertAction(title: "아니요", style: .cancel, handler: nil))
+
+        //complete handler에 화면 이동 및 리스트 전달 메서드 추가
+        sheet.addAction(UIAlertAction(title: "네", style: .default, handler: nil))
+
+        present(sheet, animated: true)
+        //navigation to purchased list page with list
+    }
 }
 
+extension BasketViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = basketCollectionView.dequeueReusableCell(withReuseIdentifier: BasketCollectionViewCell.cellId, for: indexPath) as! BasketCollectionViewCell
+
+        cell.imageView.image = UIImage(systemName: "heart.fill")
+        cell.nameLabel.text = "수박바"
+        cell.countLabel.text = "2 개"
+
+        return cell
+    }
+
+}
